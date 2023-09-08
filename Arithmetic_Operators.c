63bd5f2907eb3d590ab4b7727185188e678e4b85
Kapilchapagain <125624289+kapil-2059@users.noreply.github.com>
2023-09-08 20:15:29 +0545

    Create Arithmetic_Operators.c

---
+++ Arithmetic_Operators.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=10;
+	int b=5;
+	int c;
+	c=a+b;
+	printf("Addition:%d\n",c);
+	c=a-b;
+	printf("Subtraction:%d\n",c);
+	c=a*b;
+	printf("Multiplication:%d\n",c);
+	c=a/b;
+	printf("Division:%d\n",c);
+	c=a%b;
+	printf("Modulus:%d",c);
+return 0;
+}
+
